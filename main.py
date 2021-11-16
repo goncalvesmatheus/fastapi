9cbe7de1294053bf2bc1ed41082c3c24ef7a552f
Matheus Gonçalves <ms.goncalves98@gmail.com>
2021-11-16 19:55:39 +0000

    [UPDATE] raising HTTP exception for delete method

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import List
 from uuid import UUID, uuid4
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from models import User, Gender, Role
 
 app = FastAPI()
@@ -45,3 +45,7 @@ async def delete_user(user_id: UUID):
         if user.id == user_id:
             db.remove(user)
             return db
+    raise HTTPException(
+        status_code=404,
+        detail=f"user with id: {user_id} does not exist."
+    )
