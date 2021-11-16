ab742bacf300373e864985ed2a5e118ee3e094ab
Matheus Gonçalves <ms.goncalves98@gmail.com>
2021-11-16 18:42:53 +0000

    [UPDATE] new database and API usage

---
+++ main.py
@@ -0,0 +1,39 @@
+from typing import List
+from uuid import UUID, uuid4
+from fastapi import FastAPI
+from models import User, Gender, Role
+
+app = FastAPI()
+
+db: List[User] = [
+    User(
+        id=UUID("17b102b9-2aec-4915-9ab1-15412ca91a69"),
+        first_name="Jamila",
+        last_name="Ahmed",
+        gender=Gender.female,
+        roles=[Role.student]
+    ),
+    User(
+        id=UUID("5782f2ec-8c57-4b9e-b978-d96838989b7d"),
+        first_name="Alex",
+        last_name="Jones",
+        gender=Gender.male,
+        roles=[Role.admin, Role.user]
+    )
+]
+
+
+@app.get("/")
+async def root():
+    return {"Hello": "Mundo"}
+
+
+@app.get("/api/v1/users")
+async def fetch_users():
+    return db
+
+
+@app.post("/api/v1/users")
+async def register_user(user: User):
+    db.append(user)
+    return {"id": user.id}
